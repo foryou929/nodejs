92fe3b81fc2f6c5c4d7bff2e011a539b99efa909
foryou929 <zxcvwert3457@gmail.com>
2024-06-18 01:20:55 +0900

    fix

--- index.js
+++ index.js
@@ -2,21 +2,14 @@ const express = require('express');
 const formidable = require('formidable');
 const path = require('path');
 const cors = require('cors');
-const fs = require('fs');
 
 const app = express();
 
 app.use(cors());
 
 app.post('/api/upload', async (req, res) => {
-  const uploadsDir = path.join(process.cwd(), 'public', 'uploads');
-
-  if (!fs.existsSync(uploadsDir)) {
-    fs.mkdirSync(uploadsDir, { recursive: true });
-  }
-
   const form = new formidable.IncomingForm();
-  form.uploadDir = path.join(process.cwd(), 'public', 'uploads');
+  form.uploadDir = path.join(__dirname, 'public');
   form.keepExtensions = true;
 
   form.parse(req, async (err, fields, files) => {
@@ -26,8 +19,7 @@ app.post('/api/upload', async (req, res) => {
     }
 
     const fileExtension = path.extname(files.file[0].originalFilename);
-    const filePath = path.join('/uploads', `${files.file[0].newFilename}${fileExtension}`);
-    return res.status(200).json({ url: filePath });
+    return res.status(200).json({ url: `${files.file[0].newFilename}${fileExtension}` });
   });
 });
 
